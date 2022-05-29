31a4532a6927c109303115f4f25ccd0e36a6713c
Abhishek Kumar <anu250867anu@gmail.com>
2022-05-29 17:45:58 +0530

    added comments

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,9 +4,11 @@ import { initialballoons } from "../randomColor";
 import InputContainer from "./InputContainer";
 
 const Home = () => {
+  // define states for handling the chnages in states of balloons and text input by user
   const [balloons, setBalloons] = useState(initialballoons);
   const [inputText, setInputText] = useState();
 
+  // function that handles the input change
   const handleInputChange = (e) => {
     const text = Number(e.target.value);
 
@@ -14,12 +16,13 @@ const Home = () => {
     else alert("Please enter a number between 1 to 5");
   };
 
+  // function that handles the button click
   const handleShoot = () => {
+    // filtering balloons which are already added to the empty div and shows an alert
     const validate = balloons.filter((item) => item.id === inputText);
-    console.log("validate:", validate);
-
     if (validate[0].status === false) alert("Added Already");
 
+    // changing the status of target balloons and set the current state of balloons
     const newballoons = balloons.map((item) =>
       item.id == inputText ? { ...item, status: false } : item
     );
@@ -28,6 +31,7 @@ const Home = () => {
     setInputText("");
   };
 
+  // this function target the empty div balloos and change their status and set the state
   const handleEmptyBox = (id) => {
     const newballoons = balloons.map((item) =>
       item.id == id ? { ...item, status: true } : item
@@ -42,6 +46,7 @@ const Home = () => {
         <div>
           <h1>Empty</h1>
           <div className="empty_container">
+            {/* mapping of balloons in empty container based on their status, it only map those balloons whose status are false */}
             {balloons
               .filter((item) => item.status === false)
               .map((item) => {
@@ -55,7 +60,8 @@ const Home = () => {
         </div>
 
         <div>
-          <h1>balloons</h1>
+          <h1>Balloons</h1>
+          {/* mapping of balloons based on their status, it only map those balloons whose status are true */}
           {balloons
             .filter((item) => item.status === true)
             .map((item) => {
